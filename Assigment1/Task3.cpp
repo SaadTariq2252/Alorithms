4c6b120f4905bd061a5d391d92cc458d7b31b986
SaadTariq2252 <SaadTariq2252@gmail.com>
2024-09-02 00:20:26 +0500

    Task3 done

---
+++ Assigment1/Task3.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+int EuclidGcd(int num1, int num2);
+
+int main()
+{
+    int a=252;
+    int b=105;
+    int GCD=EuclidGcd(a,b);
+    cout<<"GCD of "<< a <<" and "<< b <<" is "<<GCD;
+
+ return 0;
+}
+
+int EuclidGcd(int num1, int num2)
+{
+    int a = num1>num2?num1:num2;
+    int b = num1>num2?num2:num1;
+    int r=-1;
+
+    while(r!=0)
+    {
+        r=a%b;
+        a=b;
+        b=r;
+    }
+    return a;
+}
