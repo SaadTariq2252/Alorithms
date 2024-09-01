62a2b5e8d9206bc38c937bc0980190fbeabba3b3
SaadTariq2252 <SaadTariq2252@gmail.com>
2024-09-02 00:56:47 +0500

    Task3 updated

--- Assigment1/Task3.cpp
+++ Assigment1/Task3.cpp
@@ -3,12 +3,16 @@ using namespace std;
 
 int EuclidGcd(int num1, int num2);
 
+
 int main()
 {
+    system("clear");
+    // int a=14142;
+    // int b=31415; 
     int a=252;
     int b=105;
-    int GCD=EuclidGcd(a,b);
-    cout<<"GCD of "<< a <<" and "<< b <<" is "<<GCD;
+    int euclidGCD=EuclidGcd(a,b);
+    cout<<"\neuclidGCD of "<< a <<" and "<< b <<" is "<<euclidGCD;
 
  return 0;
 }
