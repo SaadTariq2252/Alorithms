bd91a6bcad733146a1665769c6b3eebaf20d2f4a
SaadTariq2252 <SaadTariq2252@gmail.com>
2024-09-02 23:21:15 +0500

    Task17 done

--- Assignment1/Task17.cpp
+++ Assignment1/Task17.cpp
@@ -1,9 +1,43 @@
 #include<iostream>
 using namespace std;
 
+bool IsAnagram(string word1, string word2);
+
 int main()
 {
+    system("clear");
+    string w1="eat";
+    string w2="cat";
+    bool anagram = IsAnagram(w1,w2);
+    string res= anagram?"true":"false";
+    cout<<w1<<" And "<<w2<<" are anagrams: "<<res<<endl<<endl;
 
 
  return 0;
+}
+
+bool IsAnagram(string word1, string word2)
+{  
+    if(word1.length()!=word2.length())
+    {   
+        return false;
+    }
+    int count=0;
+    int len=word1.length();
+    for (int i = 0; i < len; i++)
+    {
+        for (int j = 0; j < len; j++)
+        {
+            if(word1[i]==word2[j])
+            {
+                count++;
+            }
+        }
+            
+    }
+    if(count!=len)
+        return false;
+
+    return true;
+
 }
